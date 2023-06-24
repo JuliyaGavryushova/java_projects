deb3f1339cca33552e88e4d037e6c9aa4b37d617
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-06-24 16:15:34 +0200

    Added longest_str

---
+++ longest_str.java
@@ -0,0 +1,31 @@
+// Напишите метод, который находит самую длинную строку общего префикса среди массива строк.
+// Если общего префикса нет, вернуть пустую строку
+
+public class longest_str {
+    public static void main(String[] args) {
+        String[] str = { "flower", "flow", "floight" };
+        System.out.println(prefix(str));
+    }
+
+    static String prefix(String[] data) {
+        String result = "";
+        Character ch;
+        int index = 0;
+        while (true) {
+            if (index == data[0].length()) {
+                return result;
+            }
+            ch = data[0].charAt(index);
+            for (String str : data) {
+                if (index == str.length()) {
+                    return result;
+                }
+                if (ch != str.charAt(index)) {
+                    return result;
+                }
+            }
+            result += ch;
+            index++;
+        }
+    }
+}
