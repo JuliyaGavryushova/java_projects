6c191bd32b662228e5f47e67b6f0d23377559e96
JuliyaGavryushova <127938887+JuliyaGavryushova@users.noreply.github.com>
2023-07-12 13:49:55 +0200

    Added Home_work_5

--- Home_work_5.java
+++ Home_work_5.java
@@ -4,40 +4,45 @@
 //  как одного человека с разными телефонами. Вывод должен быть отсортирован по убыванию числа телефонов.
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Home_work_5 {
     public static void main(String[] args) {
-        Map<String, String> phoneBook = new HashMap<>();
-        phoneBook.put("8900055555", "Иванов");
-        phoneBook.put("8900055554", "Петров");
-        phoneBook.put("8900055553", "Иванов");
-        phoneBook.put("8900055552", "Сидоров");
-        phoneBook.put("8900055551", "Васильев");
-        phoneBook.put("8900055550", "Васильев");
-        phoneBook.put("8900055558", "Иванов");
+        HashMap<String, List<String>> phoneBook = new HashMap<>();
+        addContact(phoneBook, "Иванов", "8900055555");
+        addContact(phoneBook, "Петров", "8900055554");
+        addContact(phoneBook, "Иванов", "8900055553");
+        addContact(phoneBook, "Сидоров", "8900055552");
+        addContact(phoneBook, "Васильев", "8900055551");
+        addContact(phoneBook, "Васильев", "8900055550");
+        addContact(phoneBook, "Иванов", "8900055558");
 
-        List<String> values = new ArrayList<>(phoneBook.values());
-        Collections.sort(values);
+        sortedPhoneBook(phoneBook);
+    }
 
-        LinkedHashMap<String, String> sortedMap = new LinkedHashMap<>();
-
-        for (String value : values) {
-            for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-                if (entry.getValue().equals(value)) {
-                    sortedMap.put(entry.getKey(), entry.getValue());
-                }
-            }
+    public static void addContact(HashMap<String, List<String>> phoneBook, String name, String number) {
+        if (phoneBook.containsKey(name)) {
+            List<String> numbers = phoneBook.get(name);
+            numbers.add(number);
+        } else {
+            List<String> numbers = new ArrayList<>();
+            numbers.add(number);
+            phoneBook.put(name, numbers);
         }
+    }
 
-        for (Map.Entry<String, String> entry : sortedMap.entrySet()) {
-            System.out.println("Номер телефона: " + entry.getKey() + ", Фамилия: " +
-                    entry.getValue());
+    public static void sortedPhoneBook(HashMap<String, List<String>> phoneBook) {
+        List<Map.Entry<String, List<String>>> list = new ArrayList<>(phoneBook.entrySet());
+        list.sort((entry1, entry2) -> entry2.getValue().size() - entry1.getValue().size());
+
+        for (Map.Entry<String, List<String>> entry : list) {
+            String name = entry.getKey();
+            List<String> numbers = entry.getValue();
+            String listNumbers = numbers.toString();
+            listNumbers = listNumbers.substring(1, listNumbers.length() - 1);
+            System.out.println("Имя: " + name + ", Телефоны: " + listNumbers);
         }
-
     }
 }
